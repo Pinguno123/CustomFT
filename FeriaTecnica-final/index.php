0c6493d1f7560dece4e1777beaa64c9b7496029f
Pinguno123 <douglasanchez.7b@gmail.com>
2023-07-03 15:56:14 -0600

    a

---
+++ FeriaTecnica-final/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="recursos/css/main.css">
+    <title>Feria tecnica</title>
+</head>
+<body>
+    <header>
+        <?php include("navbar.php"); ?>
+        <div class="cabecera">
+            <h1 class="titulo">
+                Chemical Learning
+            </h1>
+            <p class="descripcion">¡Bienvenido al laboratorio virtual de diversión química! Prepara tu fórmula ganadora y sumérgete en una experiencia llena de desafíos científicos, donde los juegos te llevarán a descubrir la magia de los elementos y las reacciones químicas. ¿Estás listo para experimentar la emoción de jugar con la ciencia? ¡Únete a nosotros y desata tu lado químico en este fascinante mundo de juegos!</p>
+        </div>
+    </header>
+    <main class="contenedor">
+        <section class="primerafila">
+            <div class="texto">
+                <h3>Juegos de Química</h3>
+            </div>
+            <div class="imagenes1f">
+                <a href="juegos/douglas/douglasJ.php"><img src="recursos/img/1.png" alt="Juego 1" class="imagenes"></a>
+                <a href="juegos/salvador/molec.php"><img src="recursos/img/2.png" alt="Juego 2" class="imagenes"></a>
+                <a href="juegos/amilcar/Tperiodica.php"><img src="recursos/img/3.png" alt="Juego 3" class="imagenes"></a>
+            </div>
+        </section>        
+        <section class="segundafila">
+            <div class="imagenes1f">
+                <a href="juegos/anderson/andersonJ.php"target="_blank"><img src="recursos/img/4.png" alt="Juego 4" class="imagenes"></a>
+                <a href="juegos/cesar/cesarJ.php"><img src="recursos/img/5.png" alt="Juego 5" class="imagenes"></a>
+                <a href="juegos/dana/danaJ.php"target="_blank"><img src="recursos/img/6.png" alt="Juego 6" class="imagenes"></a>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
---
+++ FeriaTecnica-final/navbar.php
@@ -0,0 +1,39 @@
+<?php $url_base = "http://localhost/FeriaTecnica-final/" ?>
+<style>
+nav {
+    padding: 20px;
+    display: flex;
+    align-items: baseline;
+    justify-content: space-between;
+    position: sticky;
+    top: 0;
+    background: white;
+}
+nav > h1 {
+    font-weight: 200;
+}
+nav > .links > a {
+    margin-right: 10px;
+    text-decoration: none;
+    color: black;
+}
+nav > .logo > a {
+    color: black;
+    text-decoration: none;
+}
+</style>
+<nav>
+    <div class="logo">
+        <a href="<?php echo $url_base?>">
+            <h1>Chemical Learning</h1>
+        </a>
+    </div>
+    <div class="links">
+        <a href="<?php echo $url_base?>juegos/douglas/douglasJ.php">Juego 1</a>
+        <a href="<?php echo $url_base?>juegos/salvador/molec.php">Juego 2</a>
+        <a href="<?php echo $url_base?>juegos/amilcar/Tperiodica.php">Juego 3</a>
+        <a href="<?php echo $url_base?>juegos/anderson/andersonJ.php" target="_blank">Juego 4</a>
+        <a href="<?php echo $url_base?>juegos/cesar/cesarJ.php">Juego 5</a>
+        <a href="<?php echo $url_base?>juegos/dana/danaJ.php" target="_blank">Juego 6</a>
+    </div>
+</nav>
